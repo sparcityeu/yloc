a829e40be933f528282cd9b1d96774946ff56776
Daniel Diefenthaler <diefenthaler@nm.ifi.lmu.de>
2022-03-17 20:45:15 +0100

    Add cmake logic to allow static linking against submodules

--- src/include/modules.h
+++ src/include/modules.h
@@ -4,3 +4,8 @@ void list_modules();
 
 // COM like module selection?
 // Return pointer to object with virtual functions?
+//
+// No dynamic linkage as found with COM
+// Not practical in HPC environment and serious security risk
+// Instead use static linked libraries,
+// Probably still with virtual functions to ease addtion of new modules
