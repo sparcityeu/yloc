61880dad87c516957387c86fc242794cf82a83c1
Daniel Diefenthaler <diefenthaler@nm.ifi.lmu.de>
2022-03-25 16:41:28 +0100

    Adjust interface to allow multiple implentations of it

--- src/include/modules/interface.h
+++ src/include/modules/interface.h
@@ -2,26 +2,34 @@
 
 typedef void* graph_t; // Boost graph here? Yes actual type here, probably Boost graph containing a map.
 
-void init_graph(graph_t * graph);               // init complete graph
-void init_graph_secondary(graph_t * graph);     // init of secondary module (if graph already is initialized by another module)
+class YlocModule {
 
-void export_graph(graph_t * graph, void * output);
-// optional function, not ever module requires this
-// it is recommended that every module that can init a graph on its own also provides this function.
+public:
 
-void update_graph(graph_t * graph);
-// Manual(?) trigger for an update on the graph
-// Thread logic in module or in init?
-// Probably both, modules that require push logic need internal threads.
-// Otherwise keep thread logic in init or modules
-//
-// An alternative would be to add a auto-update function to the interface,
-// this would be a good place to add an per-module interval
+    virtual void init_graph(graph_t * graph) = 0;               // init complete graph
+    virtual void init_graph_secondary(graph_t * graph) = 0;     // init of secondary module (if graph already is initialized by another module)
+
+    // Specifies wheter init_graph is implemented and if the module can be used standalone // todo better comment
+    // Todo better module capability system, main, export, minimal, full
+    virtual bool is_main_module() = 0;
+
+    virtual void export_graph(graph_t * graph, void ** output) = 0;
+    // optional function, not ever module requires this
+    // it is recommended that every module that can init a graph on its own also provides this function.
+
+    virtual void update_graph(graph_t * graph) = 0;
+    // Manual(?) trigger for an update on the graph
+    // Thread logic in module or in init?
+    // Probably both, modules that require push logic need internal threads.
+    // Otherwise keep thread logic in init or modules
+    //
+    // An alternative would be to add a auto-update function to the interface,
+    // this would be a good place to add an per-module interval
+
+};
 
 
 template <typename T>
 T query_graph(graph_t * graph, void * query);
 // Todo this function does not belong here,
-// it is only wriiten here to highlight the logic required inside the modules for this function
-
-
+// it is only written here to highlight the logic required inside the modules for this function
