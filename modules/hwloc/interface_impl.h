04ba7cdd819fc4b637c797430fe5943ce4a15446
Sergej Breiter <Sergej.Breiter@nm.ifi.lmu.de>
2022-04-09 11:16:51 +0200

    add hwloc api and abi compatibility features

--- modules/hwloc/interface_impl.h
+++ modules/hwloc/interface_impl.h
@@ -7,11 +7,30 @@
 // todo probably use centralized structure to maintain compatibility between modules
 //typedef graph_t void *;
 
+// enum aliases for backwards compatibility from hwloc documentation
+#if HWLOC_API_VERSION < 0x00010b00
+    #define HWLOC_OBJ_NUMANODE HWLOC_OBJ_NODE
+    #define HWLOC_OBJ_PACKAGE HWLOC_OBJ_SOCKET
+#endif /* HWLOC_API_VERSION */
+
 class YlocHwloc : public YlocModule {
 
 public:
 
     void init_graph(graph_t * graph) {               // init complete graph
+
+        // runtime check for matching hwloc abi from hwloc documentation
+        // TODO: probably move that piece of code to constructor of hwloc module
+        unsigned version = hwloc_get_api_version();
+        if ((version >> 16) != (HWLOC_API_VERSION >> 16)) {
+            fprintf(stderr,
+               "%s compiled for hwloc API 0x%x but running on library API 0x%x.\n"
+               "You may need to point LD_LIBRARY_PATH to the right hwloc library.\n"
+               "Aborting since the new ABI is not backward compatible.\n",
+               __func__, HWLOC_API_VERSION, version);
+            exit(EXIT_FAILURE);
+        }
+
         // Todo proper lifetime of this object
         *graph = init_graph_myloq("0"); // Todo use namespaces
         return;
