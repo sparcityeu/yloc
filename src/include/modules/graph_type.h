783d41b177a2411d3638217a082819357d5965b4
Sergej Breiter <Sergej.Breiter@nm.ifi.lmu.de>
2022-09-12 20:35:58 +0200

    Fix double hashmap search in add_vertex

--- src/include/modules/graph_type.h
+++ src/include/modules/graph_type.h
@@ -47,7 +47,7 @@ namespace yloc
             Vertex,                // vertex property
             // edge property (see bundled properties, subgraph)
             boost::property<boost::edge_index_t, std::size_t, Edge>>>;
-#else    
+#else
     using boost_graph_t = boost::adjacency_list<
         boost::vecS,           //  out-edge container selector
         boost::vecS,           //  Vector container selector
@@ -62,99 +62,106 @@ namespace yloc
     class Graph
     {
     public:
-
         using identifier_t = std::string;
 
         Graph() = default;
         Graph(Graph &) = default;
         Graph(Graph &&) = default;
-        
+
         Graph(boost_graph_t _graph)
-        : m_graph(_graph) {}
+            : m_graph(_graph) {}
 
         ~Graph() = default;
 
-        Graph & operator=(Graph &) = default;
-        Graph & operator=(Graph &&) = default;
+        Graph &operator=(Graph &) = default;
+        Graph &operator=(Graph &&) = default;
 
-        operator boost_graph_t&() { return m_graph; }
-        operator const boost_graph_t&() const { return m_graph; }
+        operator boost_graph_t &() { return m_graph; }
+        operator const boost_graph_t &() const { return m_graph; }
 
         /**
          * @brief Provides access to the underlying boost graph.
          */
-        boost_graph_t& boost_graph() { return m_graph; }
+        boost_graph_t &boost_graph() { return m_graph; }
 
         /**
          * @brief Adds a new unnamed vertex to the graph.
          */
-        vertex_descriptor_t add_vertex() {
+        vertex_descriptor_t add_vertex()
+        {
             return boost::add_vertex(m_graph);
         }
 
         /**
          * @brief Get a vertex for the identifier id.
-         * 
+         *
          * Adds a new vertex to the graph if there is no vertex with id.
          * Otherwise the already existing vertex is returned.
-         * 
+         *
          * @param id The identifier of the vertex.
          */
-        vertex_descriptor_t add_vertex(identifier_t id) {
-            if(m_identifier_map.count(id) > 0) {
-                return m_identifier_map.at(id);
+        vertex_descriptor_t add_vertex(identifier_t id)
+        {
+            auto iter = m_identifier_map.find(id);
+            if (iter != m_identifier_map.end()) {
+                return iter->second;
             }
             auto vd = boost::add_vertex(m_graph);
-            m_identifier_map.insert({id,vd});
-            register_pending(vd,id);
+            m_identifier_map.insert({id, vd});
+            // m_identifier_map[id] = vd; // alternative to insert
+            // register_pending(vd,id);
             return vd;
         }
 
+        /** TODO: remove register_vertex and register_pending ? */
+
         /**
          * @brief Registers an existing vertex vd with id.
-         * 
+         *
          * TODO: May depend on module initialization order
          *       If possible avoid register_vertex where possible.
          *       May introduce multiple vertices that are supposed to have the same identifier.
          *       Better: globally valid identifiers
-         * 
+         *
          * @param vd The vertex to be registered.
          * @param id The new identifier for the vertex
          * @return false if there is already another vertex with id
          */
-        bool register_vertex(vertex_descriptor_t vd, identifier_t id) {
-            auto result = m_identifier_map.insert({id,vd});
-            register_pending(vd,id);
+        bool register_vertex(vertex_descriptor_t vd, identifier_t id)
+        {
+            auto result = m_identifier_map.insert({id, vd});
+            register_pending(vd, id);
             return result.second;
         }
 
         /**
          * @brief Makes a vertex identified by old_id also available as id.
-         * 
+         *
          * TODO: May depend on module initialization order
          *       If possible avoid register_vertex where possible.
          *       May introduce multiple vertices that are supposed to have the same identifier.
          *       Better: globally valid identifiers
-         * 
+         *
          * @param old_id The identifier that the vertex is know under beforehand
          * @param id The new identifier for the vertex
          * @return false if there is no vertex with old_id or there already is another vertex with id
          */
-        bool register_vertex(identifier_t old_id, identifier_t id) {
-            if(m_identifier_map.count(old_id) == 0) {
-                m_pending_identifiers.insert({old_id,id});
+        bool register_vertex(identifier_t old_id, identifier_t id)
+        {
+            if (m_identifier_map.count(old_id) == 0) {
+                m_pending_identifiers.insert({old_id, id});
                 return false;
             }
             auto vd = m_identifier_map.at(old_id);
-            auto result = m_identifier_map.insert({id,vd});
-            register_pending(vd,id);
+            auto result = m_identifier_map.insert({id, vd});
+            register_pending(vd, id);
             return result.second;
         }
 
         /**
          * @brief Provides access to vertices of the underlying boost graph.
          */
-        auto & operator[] (vertex_descriptor_t vd)
+        auto &operator[](vertex_descriptor_t vd)
         {
             return m_graph[vd];
         }
@@ -162,7 +169,7 @@ namespace yloc
         /**
          * @brief Provides access to vertices of the underlying boost graph using an identifier.
          */
-        auto & operator[] (identifier_t id)
+        auto &operator[](identifier_t id)
         {
             /** TODO: Error handling when id is not available, maybe call add_vertex in background */
             return m_graph[m_identifier_map[id]];
@@ -173,19 +180,19 @@ namespace yloc
 #endif
 
     private:
-
         /**
          * @brief Registration of pending identifiers for vertices that only became available later.
          * Has to be called after every action that can introduce a new id to the identifier map.
-         * 
+         *
          * TODO: Warn if pending list isn't empty once all modules are initialized (end of init-function)
-         * 
+         *
          * @param vd Vertex descriptor of the node identified by id
          * @param id Identifier that became available just now
          */
-        void register_pending(vertex_descriptor_t vd, identifier_t id) {
+        void register_pending(vertex_descriptor_t vd, identifier_t id)
+        {
             auto pending_it = m_pending_identifiers.find(id);
-            while(pending_it != m_pending_identifiers.end()) {
+            while (pending_it != m_pending_identifiers.end()) {
                 auto new_id = pending_it->second;
                 m_pending_identifiers.erase(pending_it);
                 /** TODO: if this returns false there may be a duplicated node */
@@ -194,11 +201,9 @@ namespace yloc
         }
 
         boost_graph_t m_graph;
-        std::unordered_map<identifier_t,vertex_descriptor_t> m_identifier_map;
-        std::unordered_multimap<identifier_t,identifier_t> m_pending_identifiers;
+        std::unordered_map<identifier_t, vertex_descriptor_t> m_identifier_map;
+        std::unordered_multimap<identifier_t, identifier_t> m_pending_identifiers;
     };
 
     using graph_t = Graph;
 }
-
-//using yloc::graph_t; /** FIXME: do not use yloc::graph_t by default **/
