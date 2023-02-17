0a2c9dda379615f4b4d1bcf4283a7eccb2bfc46e
Sergej Breiter <Sergej.Breiter@nm.ifi.lmu.de>
2023-02-17 13:13:13 +0100

    fix mpi example

--- example/mpi/main.cc
+++ example/mpi/main.cc
@@ -8,33 +8,32 @@
 
 using namespace std::string_literals;
 
-static std::vector<std::vector<int>> find_distances(yloc::Graph &g)
+// this example finds the distances from mpi rank vertices in filtered graph to other vertices in graph
+static std::vector<std::pair<int, std::vector<int>>> find_distances(yloc::Graph &graph, std::function<bool(const yloc::vertex_descriptor_t &)> predicate)
 {
-    // this example finds the distances (#hops as metric) between MPI processes
-    std::function<bool(const yloc::vertex_descriptor_t &)> predicate = [&](const yloc::vertex_descriptor_t &v) -> bool {
-        return g[v].type->is_a<yloc::MPIProcess>();
-    };
-    auto graph_view = boost::make_filtered_graph(g.boost_graph(), boost::keep_all{}, predicate);
+    auto graph_view = boost::make_filtered_graph(graph.boost_graph(), boost::keep_all{}, predicate);
 
-    size_t num_vertices = yloc::num_vertices_view(graph_view);
-    std::cout << "number of MPI processes: " << num_vertices << std::endl;
-    if (num_vertices < 1) {
+    size_t num_vertices_view = yloc::num_vertices_view(graph_view);
+    std::cout << "number of MPI processes: " << num_vertices_view << std::endl;
+    if (num_vertices_view < 1) {
         return {};
     }
 
     int i = 0;
-    std::vector<std::vector<int>> distance_matrix(num_vertices);
+    std::vector<std::pair<int, std::vector<int>>> distance_matrix(num_vertices_view);
 
-    std::for_each(boost::vertices(graph_view).first, boost::vertices(graph_view).second, [&](const yloc::vertex_descriptor_t &v) {
-        distance_matrix[i] = std::vector<int>(num_vertices);
-        auto dist_pmap = boost::make_iterator_property_map(distance_matrix[i].begin(), get(boost::vertex_index, graph_view));
+    std::for_each(boost::vertices(graph_view).first, boost::vertices(graph_view).second, [&](const yloc::vertex_descriptor_t &vd) {
+        distance_matrix[i] = {graph[vd].get("mpi_rank").value(), std::vector<int>(boost::num_vertices(graph.boost_graph()))};
+        auto dist_pmap = boost::make_iterator_property_map(distance_matrix[i].second.begin(), boost::get(boost::vertex_index, graph.boost_graph()));
         auto visitor = boost::make_bfs_visitor(boost::record_distances(dist_pmap, boost::on_tree_edge()));
-        boost::breadth_first_search(graph_view, v, boost::visitor(visitor));
+        boost::breadth_first_search(graph.boost_graph(), vd, boost::visitor(visitor));
+        ++i;
     });
 
     return distance_matrix;
 }
 
+// this example finds the distances (#hops as metric) between MPI processes
 int main(int argc, char *argv[])
 {
     MPI_Init(&argc, &argv);
@@ -44,11 +43,20 @@ int main(int argc, char *argv[])
 
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int p = 0;
+
+    std::function<bool(const yloc::vertex_descriptor_t &)> predicate = [&](const yloc::vertex_descriptor_t &v) -> bool {
+        return g[v].type->is_a<yloc::MPIProcess>();
+    };
+    auto graph_view = boost::make_filtered_graph(g.boost_graph(), boost::keep_all{}, predicate);
 
     if (rank == 0) {
-        std::vector<std::vector<int>> distance_matrix = find_distances(g);
-        std::for_each(distance_matrix.begin(), distance_matrix.end(), [&](auto d) {
-            std::for_each(d.begin(), d.end(), [=](int i) { std::cout << i << '\n'; });
+        auto distance_matrix = find_distances(g, predicate);
+        std::for_each(distance_matrix.begin(), distance_matrix.end(), [&](const auto &pair) {
+            std::cout << "distance from mpi rank " << pair.first << '\n';
+            for(const auto &vd : boost::make_iterator_range(boost::vertices(graph_view))) {
+                std::cout << "to " << g[vd].get("mpi_rank").value() << ": " << pair.second[vd] << '\n';
+            }
         });
 
         yloc::write_graph_dot_file(g, "mpi_graph.dot"s, std::vector{"mpi_rank"s});
