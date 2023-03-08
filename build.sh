c30c353c5bbfd0838358a1d738b672e3a99eb1d3
Sergej Breiter <Sergej.Breiter@nm.ifi.lmu.de>
2023-03-08 11:32:18 +0100

    Move compiler warnings from CMakeLists to build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source scripts/env.sh
-mkdir -p build && cd build && cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=TRUE && make && cd ..
-
+mkdir -p build && cd build && cmake .. -Wdev -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=TRUE && cmake --build . -j
+cd ..
