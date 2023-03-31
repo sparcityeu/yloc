01c294b9147a74fcd13044309512cb88a10123ac
Sergej Breiter <Sergej.Breiter@nm.ifi.lmu.de>
2023-03-31 13:53:19 +0200

    Refactor distance query (usable with any invocable)

--- src/include/yloc/query.h
+++ src/include/yloc/query.h
@@ -5,64 +5,58 @@
 #include <boost/graph/breadth_first_search.hpp>
 
 #include <functional>
+#include <type_traits>
+#include <vector>
 
 namespace yloc
 {
-    template <class DistMap>
+    template <class Graph>
+    using vertex = typename boost::graph_traits<Graph>::vertex_descriptor;
+
+    template <class Graph>
+    using edge = typename boost::graph_traits<Graph>::edge_descriptor;
+
+    template <class Graph, class DistanceMap, class Fn>
     class bfs_tree_edge_visitor : public boost::default_bfs_visitor
     {
-        typedef typename boost::property_traits<DistMap>::value_type T;
+        using T = typename boost::property_traits<DistanceMap>::value_type;
+        using E = edge<Graph>;
 
     public:
-        bfs_tree_edge_visitor(DistMap dmap, std::function<T(const Graph &, edge_descriptor_t, T)> on_tree_edge)
-            : m_distmap{dmap}, m_on_tree_edge{on_tree_edge} {}
-
-        void tree_edge(edge_descriptor_t e, const Graph &g) const
+        bfs_tree_edge_visitor(Graph &graph, DistanceMap dmap, Fn on_tree_edge)
+            : m_distmap{dmap}, m_on_tree_edge{on_tree_edge}
         {
-            typename boost::graph_traits<Graph>::vertex_descriptor u = boost::source(e, g),
-                                                                   v = boost::target(e, g);
-            boost::put(m_distmap, v, m_on_tree_edge(g, e, boost::get(m_distmap, u)));
+        }
+
+        void tree_edge(E e, const Graph &graph) const
+        {
+            vertex<Graph> u = boost::source(e, graph);
+            vertex<Graph> v = boost::target(e, graph);
+            boost::put(m_distmap, v, m_on_tree_edge(graph, e, boost::get(m_distmap, u)));
         }
 
     private:
-        DistMap m_distmap;
-        std::function<T(const Graph &, edge_descriptor_t, T)> m_on_tree_edge;
+        DistanceMap m_distmap;
+        Fn m_on_tree_edge;
     };
 
-    template <typename T>
-    std::vector<T> bfs_distance_vector(vertex_descriptor_t v, std::function<T(const Graph &, edge_descriptor_t, T)> on_tree_edge)
+    template <class Graph, class Fn>
+    auto bfs_distance_vector(Graph &graph, vertex<Graph> v, Fn on_tree_edge)
     {
-        auto &graph = yloc::root_graph();
+        using T = typename decltype(std::function{std::declval<Fn>()})::result_type;
+        static_assert(std::is_invocable_r_v<T, Fn, Graph &, edge<Graph>, T>);
+
         std::vector<T> dist(boost::num_vertices(graph));
         auto dist_pmap = boost::make_iterator_property_map(dist.begin(), boost::get(boost::vertex_index, graph));
-        auto visitor = bfs_tree_edge_visitor(dist_pmap, on_tree_edge);
-
+        auto visitor = bfs_tree_edge_visitor(graph, dist_pmap, on_tree_edge);
         boost::breadth_first_search(graph, v, boost::visitor(visitor));
         return dist;
     }
 
-    // number of edges in the shortest path from the source vertex
-    inline std::vector<int> bfs_distance_vector(vertex_descriptor_t v)
+    // number of edges in the shortest path from the source vertex to other vertices
+    template <class Graph>
+    std::vector<int> bfs_distance_vector(Graph &graph, vertex<Graph> v)
     {
-        // auto &graph = yloc::root_graph();
-        // std::vector<int> dist(boost::num_vertices(graph));
-        // auto dist_pmap = boost::make_iterator_property_map(dist.begin(), boost::get(boost::vertex_index, graph));
-        // auto visitor = boost::make_bfs_visitor(boost::record_distances(dist_pmap, boost::on_tree_edge()));
-        // boost::breadth_first_search(graph, v, boost::visitor(visitor));
-
-        return yloc::bfs_distance_vector(v, std::function{[&](const yloc::Graph &graph, yloc::edge_descriptor_t e, int val_pre) {
-                                             return val_pre + 1;
-                                         }});
+        return bfs_distance_vector(graph, v, [&](const Graph &g, edge<Graph> e, int val_pre) { return val_pre + 1; });
     }
-
-#if YLOC_NOT_IMPLEMENTED_YET
-    class Query
-    {
-    };
-
-    void distance(vertex_descriptor_t &vd_start, vertex_descriptor_t &vd_end)
-    {
-    }
-#endif
-
-}
+} // namespace yloc
